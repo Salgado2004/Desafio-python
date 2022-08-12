835bcb8636db7abbab99d1d1bfdd71ca2851c84d
LeonardoSalgado04 <53799801+Salgado2004@users.noreply.github.com>
2022-08-12 15:20:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ def erro404(error):
 @app.route('/', methods=['POST', 'GET'])
 def entry_page() -> 'html':
     return render_template('restaurantHome.html')
+
+@app.route('/contate-nos', methods=['POST'])
+def contactMsg() -> 'html':
+    nome = request.form["nome"]
+    email = request.form["email"]
+    msg = request.form["msg"]
+    date = datetime.datetime.now()
+    data = str(date.year)+'-'+str(date.month)+'-'+str(date.day)
+    mfc.getMsg(fnome = nome, femail = email, fmsg = msg, fdata= data)
+    return redirect(url_for('entry_page'))
   
 if __name__ == '__main__':
     app.run(debug=True)
