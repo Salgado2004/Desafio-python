4c3861708eb97d52614372e5516aede85b404ff9
LeonardoSalgado04 <53799801+Salgado2004@users.noreply.github.com>
2022-08-11 21:21:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,21 +6,7 @@ app.secret_key = 'aihfisdjfewsue'
 
 @app.errorhandler(404)
 def erro404(error):
-    return render_template('GerenciadorErro.html',
-                            the_msg = "Opa! Essa página não existe :/",
-                            the_erro = "404"), 404
-
-@app.errorhandler(400)
-def erro400(error):
-    return render_template('GerenciadorErro.html',
-                            the_msg = "Opa! Parece que falta alguma coisa no seu código :/",
-                            the_erro = "400"), 400
-
-@app.errorhandler(405)
-def erro405(error):
-    return render_template('GerenciadorErro.html',
-                            the_msg = "Opa! O método utilizado para acessar essa página não é válido :/",
-                            the_erro = "405"), 405
+    return render_template('GerenciadorErro.html'), 404
   
 @app.route('/')
 def entry_page() -> 'html':
