d95040a6f8a66d2954d1ae7d09bcf03b83f76d25
LeonardoSalgado04 <53799801+Salgado2004@users.noreply.github.com>
2022-08-12 15:21:09 -0300

    Update conectionsBD.py

--- conectionsBD.py
+++ conectionsBD.py
@@ -10,3 +10,18 @@ def log():
     res = cursor.fetchall()
     for row in res:
         return row
+  
+def saveMsg(**arg):
+    nome = arg['fnome']
+    email = arg['femail']
+    msg = arg['fmsg']
+    data= arg['fdata']
+    commit = (nome, email, msg, data)
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """insert into mensagens(nome, email, mensagem, dataMsg) values ("%s", "%s", "%s", "%s");"""
+    cursor.execute(_SQL, commit)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return "enviado ao banco de dados"
