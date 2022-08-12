30ec3d854ace23087650b039a269a26955c858f9
LeonardoSalgado04 <53799801+Salgado2004@users.noreply.github.com>
2022-08-12 15:20:33 -0300

    Update minhasFunctions.py

--- minhasFunctions.py
+++ minhasFunctions.py
@@ -1,1 +1,9 @@
 import conectionsBD as bd
+
+def getMsg(**arg):
+    nome = arg['fnome']
+    email = arg['femail']
+    msg = arg['fmsg']
+    data= arg['fdata']
+    result = bd.saveMsg(fnome = nome, femail = email, fmsg = msg, fdata= data)
+    return result
